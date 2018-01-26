501a376fd0b3d839705926060a33cbf38e8349ea
Martin Broerse <broerse@martinic.com>
2018-01-26 13:31:38 +0100

    Fix Inject in post controller

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
-// import { inject } from '@ember/controller';
 
 export default Controller.extend({
-//  posts: inject.controller()
+  posts: Controller.inject()
 });
