5f79da61cfb305544bfab223b8c4c0263f1ac187
Martin Broerse <broerse@martinic.com>
2020-01-10 04:13:00 +0100

    Update to ember-cli 3.10.1

--- config/environment.js
+++ config/environment.js
@@ -6,11 +6,10 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
-
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
+        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
