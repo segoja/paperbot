3683501c0d4ce44046a7453d0fb5a2c62f482299
Martin Broerse <broerse@martinic.com>
2020-02-01 06:33:03 +0100

    Improve Helpers

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,7 +1,6 @@
 import { helper } from '@ember/component/helper';
 import moment from 'moment';
 
-export default helper(function(params) {
-  let value = params[0];
+export default helper(function([value]) {
   return moment(value).fromNow();
 });
--- app/helpers/format-markdown.js
+++ app/helpers/format-markdown.js
@@ -2,7 +2,6 @@ import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/string';
 import marked from 'marked';
 
-export default helper(function(params) {
-  let value = params[0];
+export default helper(function([value]) {
   return htmlSafe(marked(value));
 });
