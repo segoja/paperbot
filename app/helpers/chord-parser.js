0345ed26130954d18992924875cc6d61ff515a66
segoja <segoja@gmail.com>
2023-02-21 01:25:50 +0100

    WIP: chord parsing

--- app/helpers/chord-parser.js
+++ app/helpers/chord-parser.js
@@ -1,6 +1,7 @@
 import { helper } from '@ember/component/helper';
 import { htmlSafe } from '@ember/template';
 import ChordSheetJS from 'chordsheetjs';
+import { transpose, Chord, KeySignatures } from 'chord-transposer';
 
 export default helper(function([element]) {
   if (!element) { return; } // return nothing when params is empty
@@ -46,11 +47,17 @@ export default helper(function([element]) {
       return htmlSafe(disp); 
     }   
   }*/
-  let content = element.replace(/\r\n?/g, '\n');
+  
+  let regex = new RegExp('(\\b(?:(?:A(?:b(?:[679+]|sus)?|m[67]?|[689]|maj7|dim|\\+|sus)?)|(?:B(?:b(?:[679+]|sus|m[67]?|maj7|dim)?|m[67]?|[679+]|maj7|dim|sus)?)|(?:C(?:[679+]|m[67]?|maj7|dim|sus|\#(?:m[67]?|dim)?)?)|(?:D(?:b(?:[679+]|maj7|sus)?|[679]|m[67]?|maj7|dim|\\+|sus)?)|(?:E(?:b(?:[679+]|sus|m[67]?|maj7|dim)?|m[67]?|[679+]|maj7|dim|sus)?)|(?:F(?:[679+]|m[67]?|maj7|dim|sus|\#(?:[79]|m[67]|dim)?)?)|(?:G(?:b(?:maj7|sus|[6+])?|[679+]|m[67]?|maj7|dim|sus|\#(?:m[67]?|dim))?))(?=\\s|$))', 'gmu');
+  let content = element.replace(/\#/g, '\#');
+  content = element.replaceAll(regex, '<strong>$1</strong>');
+  console.log(content);
+  //content = content.replace(regex, `<strong>$1</strong>`);
+  content = content.replace(/\r\n?/g, '\n');
   content = content.replace(/\n\s\n/g, '\n\n');
   content = content.replace(/\n/g, '<br>\n');
   let lines = content.split('<br>\n');
-  console.log(lines);
+  // console.log(lines);
   
   let processed = '';
   lines.forEach((line)=>{
